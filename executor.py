cdbe1c66994fd46f143774131ef470766fbf85ca
belousgm <kirill.trofimov03@mail.ru>
2021-10-03 01:14:37 +0300

    rename to pokemon_list

--- executor.py
+++ executor.py
@@ -1,6 +1,6 @@
 file = open('pokemon_full.json')
-all_symbols = file.read()
+pokemon_list = file.read()
 file.close()
-print('Общее количество символов:', len(all_symbols))
+print('Общее количество символов:', len(pokemon_list))
 print('Общее количество символов без пробелов и знаков препинания:',
-      len(all_symbols) - all_symbols.count(' ') - all_symbols.count(',') - all_symbols.count(':'))
+      len(pokemon_list) - pokemon_list.count(' ') - pokemon_list.count(',') - pokemon_list.count(':'))
