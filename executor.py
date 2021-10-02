3fe2e9a09a89b17c008434568b910265dfe09151
belousgm <kirill.trofimov03@mail.ru>
2021-10-03 00:56:35 +0300

    completed first and second point

--- executor.py
+++ executor.py
@@ -1,2 +1,6 @@
-a = 2
-print(a)
+file = open(r'pokemon_full.json')
+all_symbols = file.read()
+file.close()
+print('Общее количество символов:', len(all_symbols))
+print('Общее количество символов без пробелов и знаков препинания:',
+      len(all_symbols) - all_symbols.count(' ') - all_symbols.count(',') - all_symbols.count(':'))
