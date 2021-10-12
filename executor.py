6425143a56865c4a361a742c31ad1a76ed3466f3
belousgm <kirill.trofimov03@mail.ru>
2021-10-12 21:37:42 +0300

    lab1 is completed

--- executor.py
+++ executor.py
@@ -1,6 +1,36 @@
+import json
+
 file = open('pokemon_full.json')
 pokemon_list = file.read()
 file.close()
+
 print('Общее количество символов:', len(pokemon_list))
-print('Общее количество символов без пробелов и знаков препинания:',
-      len(pokemon_list) - pokemon_list.count(' ') - pokemon_list.count(',') - pokemon_list.count(':'))
+
+count_symb = 0
+for symbols in pokemon_list:
+    if symbols.isalnum():
+        count_symb += 1
+print('Общее количество символов без пробелов и знаков препинания:', count_symb)
+
+pokemon_list = json.loads(pokemon_list)
+max_description = 0
+name_pokemon = ''
+for characteristic in pokemon_list:
+    if len(characteristic["description"]) > max_description:
+        max_description = len(characteristic["description"])
+        name_pokemon = characteristic["name"]
+print('Самое длинное описание у покемона по имени', name_pokemon)
+
+max_words = 0
+max_abilities = []
+for skills in pokemon_list:
+    for skill in skills["abilities"]:
+        if len(skill.split()) > max_words:
+            max_words = len(skill.split())
+for max_skills in pokemon_list:
+    for max_skill in max_skills["abilities"]:
+        if len(max_skill.split()) == max_words:
+            max_abilities.append(max_skill)
+print('Умения, содержащие больше всего слов: ', end='')
+for ability in max_abilities:
+    print(ability, end='; ')
